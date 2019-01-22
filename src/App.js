bee39d7c479d0e1d44cfba5ea8e0d75171648e39
qcardell <42754526+qcardell@users.noreply.github.com>
2019-01-22 10:00:46 -0600

    Add Search Menu
    Added Search Menu to the HTML page.  Working on making it work.

--- src/App.js
+++ src/App.js
@@ -21,22 +21,7 @@ var locations = [
 var markers = [];
 var itemlocations = [];
 
-var params = {
-	// 40.7413549, lng: -73.9980244
-	//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
-	//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
-	"ll": "37.7749,-122.4194",
-	"query": 'food',
-	// v: "20180323",
-	//"near": "Dallas, Tx"
-	
-  
- //   "near": "Dallas, Tx",
- // "categoryId": '4d4b7105d754a06374d81259',
- // "radius":"250"
- //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>}) }
 
-};
 
 class App extends Component {
 	constructor(props) {
@@ -45,19 +30,34 @@ class App extends Component {
 			items: []
 		};
 	}
-	//componentDidMount() {   
-		//this.getVenues();
-	//	foursquare.venues.getVenues(params).then((res) => {
-	//		this.setState({
-	//			items: res.response.venues
-	//		});
-	//		console.log(this.state);
-	//	})
-		//console.log(this.state);
-	//}
+		//componentDidMount() {   
+			//this.getVenues();
+		//	foursquare.venues.getVenues(params).then((res) => {
+		//		this.setState({
+		//			items: res.response.venues
+		//		});
+		//		console.log(this.state);
+		//	})
+			//console.log(this.state);
+		//}
 	
 	getVenues = () => {
 		const endPoint = "https://api.foursquare.com/v2/venues/explore?";
+		var params = {
+			// 40.7413549, lng: -73.9980244
+			//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
+			//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
+			"ll": "37.7749,-122.4194",
+			"query": 'food',
+			// v: "20180323",
+			//"near": "Dallas, Tx"
+	
+  
+			 //   "near": "Dallas, Tx",
+			 // "categoryId": '4d4b7105d754a06374d81259',
+			 // "radius":"250"
+			 //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>})
+		};
 		
 		//axios.get(endPoint + new URLSearchParams(params))
 		//	.then(response => {
@@ -76,33 +76,60 @@ class App extends Component {
 		//console.log(this.state.items);
 	}
 	
+	updateQuery = (query) =>{
+		var params = {
+			// 40.7413549, lng: -73.9980244
+			//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
+			//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
+			"ll": "37.7749,-122.4194",
+			"query": {query},
+			// v: "20180323",
+			//"near": "Dallas, Tx"
+	
+  
+		 //   "near": "Dallas, Tx",
+		 // "categoryId": '4d4b7105d754a06374d81259',
+		 // "radius":"250"
+		 //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>}) }
+
+		};
+		//this.setState({query: query.trim()})
+		
+		foursquare.venues.getVenues(params).then((res) => {
+			//this.setState({
+			//	items: res.response.venues
+			//});
+			console.log(this.state.items);
+		})
+	}
+	
 	
 	getGoogleMaps() {
     // If we haven't already defined the promise, define it
-    if (!this.googleMapsPromise) {
-      this.googleMapsPromise = new Promise((resolve) => {
-        // Add a global handler for when the API finishes loading
-        window.resolveGoogleMapsPromise = () => {
-			const google = window.google;
-          // Resolve the promise
-          resolve(google);
+		if (!this.googleMapsPromise) {
+		  this.googleMapsPromise = new Promise((resolve) => {
+			// Add a global handler for when the API finishes loading
+			window.resolveGoogleMapsPromise = () => {
+				const google = window.google;
+			  // Resolve the promise
+			  resolve(google);
 
-          // Tidy up
-          delete window.resolveGoogleMapsPromise;
-        };
+			  // Tidy up
+			  delete window.resolveGoogleMapsPromise;
+			};
 
-        // Load the Google Maps API
-        const script = document.createElement("script");
-        const API = 'AIzaSyAOoEVFmUBvaqUgrMpSZQEGpgp1x9_y-I4';
-        script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
-        script.async = true;
-        document.body.appendChild(script);
-      });
-    }
+			// Load the Google Maps API
+			const script = document.createElement("script");
+			const API = 'AIzaSyAOoEVFmUBvaqUgrMpSZQEGpgp1x9_y-I4';
+			script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
+			script.async = true;
+			document.body.appendChild(script);
+		  });
+		}
 
     // Return a promise for the Google Maps API
-    return this.googleMapsPromise;
-  }
+		return this.googleMapsPromise;
+	}
 
   componentWillMount() {
     // Start Google Maps API loading since we know we'll soon need it
@@ -111,187 +138,224 @@ class App extends Component {
 
   componentDidMount() {
 	  
-	//Get 4 square Data.
-	foursquare.venues.getVenues(params).then((res) => {
-		this.setState({
-			items: res.response.venues
-		}, this.rendermap()
-		);
-		console.log(this.state);
-	})
+		var params = {
+			// 40.7413549, lng: -73.9980244
+			//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
+			//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
+			"ll": "37.7749,-122.4194",
+			"query": 'food',
+			// v: "20180323",
+			//"near": "Dallas, Tx"
+			
+	  
+			 //   "near": "Dallas, Tx",
+			 // "categoryId": '4d4b7105d754a06374d81259',
+			 // "radius":"250"
+			 //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>}) }
+		};
+		  
+		//Get 4 square Data.
+		foursquare.venues.getVenues(params).then((res) => {
+			this.setState({
+				items: res.response.venues
+			}, this.rendermap()
+			);
+			console.log(this.state);
+		})
 
-  }
+	}
 	
 	rendermap(){
 		    // Once the Google Maps API has finished loading, initialize the map
-    this.getGoogleMaps().then((google) => {
-		var styles =[
-			{
-				featureType: 'water',
-				stylers: [
-					{color: '#19a0d8'}
-				]
-			},{
-				featureType: 'administrative',
-				elementType: 'labels.text.stroke',
-				stylers: [
-					{color: '#ffffff'},
-					{weight: 6}
-				]
-			},{
-				featureType: 'road.highway',
-				elementType: 'geometry.stroke',
-				stylers: [
-					{color: '#efe9e4'},
-					{lightness: -40 }				
-				]
-			},{
-				featureType: 'transit.station',
-				stylers: [
-					{weight: 9},
-					{hue: '#e85113' }
-				]
-			},{
-				featureType: 'road.highway',
-				elementType: 'labels.icon',
-				stylers: [
+		this.getGoogleMaps().then((google) => {
+			var styles =[
+				{
+					featureType: 'water',
+					stylers: [
+						{color: '#19a0d8'}
+					]
+				},{
+					featureType: 'administrative',
+					elementType: 'labels.text.stroke',
+					stylers: [
+						{color: '#ffffff'},
+						{weight: 6}
+					]
+				},{
+					featureType: 'road.highway',
+					elementType: 'geometry.stroke',
+					stylers: [
+						{color: '#efe9e4'},
+						{lightness: -40 }				
+					]
+				},{
+					featureType: 'transit.station',
+					stylers: [
+						{weight: 9},
+						{hue: '#e85113' }
+					]
+				},{
+					featureType: 'road.highway',
+					elementType: 'labels.icon',
+					stylers: [
 					{visibility: 'off'}
-				]
-			},{
-				featureType: 'road.highway',
-				elementType: 'geometry.fill',
-				stylers: [
-					{color: '#efe9e4'},
-					{lightness: -25}
-				]
+					]
+				},{
+					featureType: 'road.highway',
+					elementType: 'geometry.fill',
+					stylers: [
+						{color: '#efe9e4'},
+						{lightness: -25}
+					]
 				}
-		]
-		const center = {lat: 37.7749, lng: -122.4194};
-		//		const center = {lat: 40.7413549, lng: -73.9980244};
-		//37.7749,-122.4194"
-		const map = new google.maps.Map(document.getElementById('map'), {
-			zoom: 13,
-			center: center,
-			//styles: styles,
-			mapTypeControl: false
-		});
+			]
+			const center = {lat: 37.7749, lng: -122.4194};
+				//const center = {lat: 40.7413549, lng: -73.9980244};
+				//37.7749,-122.4194"
+			const map = new google.maps.Map(document.getElementById('map'), {
+				zoom: 13,
+				center: center,
+				//styles: styles,
+				mapTypeControl: false
+			});
 	  
-		var largeInfowindow = new google.maps.InfoWindow();
+			var largeInfowindow = new google.maps.InfoWindow();
 		
-		var defaultIcon = makeMarkerIcon('0091ff');
+			var defaultIcon = makeMarkerIcon('0091ff');
 		
-		var highlightedIcon = makeMarkerIcon('FFFF24');
+			var highlightedIcon = makeMarkerIcon('FFFF24');
 		
-		//const marker = new google.maps.Marker({
-		//	position: center,
-		//	map: map
-		//});
-		
-		//var bounds = new google.maps.LatLngBounds();
-		console.log(this.state.items);
-		this.state.items.map(venue => {
-		//for (var i=0; i < locations.length; i++) {
-			//location: {lat: 40.7713024, lng: -73.9632393}
-			//var position = locations[i].location;
-			var position = {lat: venue.location.lat,lng:venue.location.lng};
+			//const marker = new google.maps.Marker({
+			//	position: center,
+			//	map: map
+			//});
+			
+			//var bounds = new google.maps.LatLngBounds();
+			//console.log(this.state.items);
+			this.state.items.map(venue => {
+			//for (var i=0; i < locations.length; i++) {
+				//location: {lat: 40.7713024, lng: -73.9632393}
+				//var position = locations[i].location;
+				var position = {lat: venue.location.lat,lng:venue.location.lng};
 
-			var title = venue.name; //locations[i].title;
-			
-			var marker = new google.maps.Marker({
-				position: position,
-				title: title,
-				icon: defaultIcon,
-				animation: google.maps.Animation.DROP,
-				id: venue.id,
-				map:map
-			});
-		
-			markers.push(marker);
-			
-			//bounds.extend(marker.position);
-			
-			marker.addListener('click', function() {
-				populateInfoWindow(this, largeInfowindow);
-			});
-			
-			marker.addListener('mouseover', function(){
-				this.setIcon(highlightedIcon);
-			});
-			marker.addListener('mouseout', function(){
-				this.setIcon(defaultIcon);
-			});
-		})
-		
-		
-		
-		function populateInfoWindow(marker, infowindow) {
-			if (infowindow.marker != marker) {
-				infowindow.setContent('');
-				infowindow.marker = marker;
+				var title = venue.name; //locations[i].title;
 				
-				infowindow.addListener('closeclick', function(){
-					infowindow.setMarker(null);
+				var marker = new google.maps.Marker({
+					position: position,
+					title: title,
+					icon: defaultIcon,
+					animation: google.maps.Animation.DROP,
+					id: venue.id,
+					map:map
 				});
-				var streetViewService = new google.maps.StreetViewService();
-				var radius = 50;
+			
+				markers.push(marker);
 				
+				//bounds.extend(marker.position);
 				
+				marker.addListener('click', function() {
+					populateInfoWindow(this, largeInfowindow);
+				});
 				
-		function getStreetView(data, status){
-				//console.log();
-					if (status == google.maps.StreetViewStatus.OK){
-						var nearStreetViewLocation = data.location.latLng;
-						var heading = google.maps.geometry.spherical.computeHeading(
-							nearStreetViewLocation, marker.position);
-						//console.log(heading);
-						infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
-						//console.log(infowindow);
-						var panoramaOptions = {
-						position: nearStreetViewLocation,
-						pov: {
-							heading: heading,
-							pitch: 30
-						}
-					};
-					var panorama = new google.maps.StreetViewPanorama(
-						document.getElementById('pano'), panoramaOptions);
-						//console.log(panorama);
-					}else {
-						//console.log(marker);
-						infowindow.setContent('<div>' + marker.title + '</div>' +
-						'<div>No Street View Found</div>');
-					}
-				}
-				streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-				
-				infowindow.open(map, marker);
-		}
+				marker.addListener('mouseover', function(){
+					this.setIcon(highlightedIcon);
+				});
+				marker.addListener('mouseout', function(){
+					this.setIcon(defaultIcon);
+				});
+			})
 		
-		}
-		function makeMarkerIcon(markerColor){
-			var markerImage = new google.maps.MarkerImage(
-				'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+markerColor+'|40|_|%E2%80%A2',
-				new google.maps.Size(21, 34),
-				new google.maps.Point(0,0),
-				new google.maps.Point(10, 34),
-				new google.maps.Size(21, 34));
-			//console.log(markerImage);
-			return markerImage;
-		}
-    });
+			function populateInfoWindow(marker, infowindow) {
+				if (infowindow.marker != marker) {
+					infowindow.setContent('');
+					infowindow.marker = marker;
+					
+					infowindow.addListener('closeclick', function(){
+						infowindow.setMarker(null);
+					});
+					var streetViewService = new google.maps.StreetViewService();
+					var radius = 50;
+					
+					
+					
+					function getStreetView(data, status){
+						//console.log();
+						if (status == google.maps.StreetViewStatus.OK){
+							var nearStreetViewLocation = data.location.latLng;
+							var heading = google.maps.geometry.spherical.computeHeading(
+								nearStreetViewLocation, marker.position);
+							//console.log(heading);
+							infowindow.setContent('<div>' + marker.title + '</div><div id="pano"></div>');
+							//console.log(infowindow);
+							var panoramaOptions = {
+								position: nearStreetViewLocation,
+								pov: {
+									heading: heading,
+									pitch: 30
+								}
+							};
+							var panorama = new google.maps.StreetViewPanorama(
+								document.getElementById('pano'), panoramaOptions);
+								//console.log(panorama);
+							}else {
+								//console.log(marker);
+								infowindow.setContent('<div>' + marker.title + '</div>' +
+								'<div>No Street View Found</div>');
+							}
+						}
+						
+						streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
+						
+						infowindow.open(map, marker);
+				}
+			}
+		
+			function makeMarkerIcon(markerColor){
+				var markerImage = new google.maps.MarkerImage(
+					'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|'+markerColor+'|40|_|%E2%80%A2',
+					new google.maps.Size(21, 34),
+					new google.maps.Point(0,0),
+					new google.maps.Point(10, 34),
+					new google.maps.Size(21, 34));
+					//console.log(markerImage);
+					return markerImage;
+			}
+		});
 		
 	}
 	
-  render() {
-    return (
-		<div className="App">
-			      <div>
-        <h1>Map</h1>
-        <div id="map"></div>
-      </div>
-		</div>
-    );
-  }
+	render() {
+		return (
+			<div className="App">
+				<div className='options-box'>
+					<h1>Options Menu</h1>
+					<div>
+						<span>Filter  </span>
+						<input 
+							id="hide-listings" 
+							type="text" 
+							placeholder='Search: ex Food'
+							onBlur={(event) => this.updateQuery(event.target.value)}
+						>
+						</input>
+						<input 
+							id="hide-listings" 
+							type="button" 
+							value="Search"
+							onChange={(event) => this.updateQuery(event.target.value)}>
+						</input>
+					</div>
+					<div>
+						<span>Lat: </span>
+						<input id="show-listings" type="input" ></input>
+						<span>  Lng: </span>
+						<input id="show-listings" type="input" ></input>
+					</div>
+				</div>
+				<div id='map'></div>
+			</div>
+		);
+	}
 }
 
 export default App;
