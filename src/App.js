87107772ff14ec30050f075669c74063d5054d22
Quinnton Cato <qcardell@outlook.com>
2019-01-24 20:33:16 -0600

    Cleanup code, and removed my keys.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import React, { Component } from 'react';
 import './App.css';
-//import ShowMap from './ShowMap';
-import axios from 'axios'; // ajax stuff similar to jquery but with promises
+//import axios from 'axios'; // ajax stuff similar to jquery but with promises
 import escapeRegExp from 'escape-string-regexp'
 
 
 var foursquare = require('react-foursquare')({
-  clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
-  clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK'  
+  clientID: '',
+  clientSecret: ''  
 });
+
+//Goole map key
+const API = '';
+//Google map key
+
 var markers = [];
 let filterMarkers = [];
-//let showingItems;
 var map ;
 var center;
 var marker;
@@ -19,7 +22,6 @@ var google;
 var largeInfowindow;
 var previousElement;
 var previouswidth;
-//var showingItems = [];
 
 class App extends Component {
 	constructor(props) {
@@ -35,20 +37,8 @@ class App extends Component {
 
 		};
 	}
-		//componentDidMount() {   
-			//this.getVenues();
-		//	foursquare.venues.getVenues(params).then((res) => {
-		//		this.setState({
-		//			items: res.response.venues
-		//		});
-		//		console.log(this.state);
-		//	})
-			//console.log(this.state);
-		//}
-	
+		
 	componentDidCatch(error, info) {
-    // Something happened to one of my children.
-    // Add error to state
 		this.setState({
 			error: error,
 			info: info,
@@ -56,57 +46,12 @@ class App extends Component {
 	}
 	
 	handleResize = () =>{
-		//this.setState({
-		//windowHeight: window.innerHeight,
-		//windowWidth: window.innerWidth
-		//});
-		//console.log(window.innerWidth);
-			if(window.innerWidth > 801 && previouswidth < 801){
-				this.rendermapOnly();
-				this.rendermap();
-				//console.log("render");
-			}
-			previouswidth=window.innerWidth;
+		if(window.innerWidth > 801 && previouswidth < 801){
+			this.rendermapOnly();
+			this.rendermap();
+		}
+		previouswidth=window.innerWidth;
 
-	}
-
-
-	getVenues = () => {
-		const endPoint = "https://api.foursquare.com/v2/venues/explore?";
-		var params = {
-			// 40.7413549, lng: -73.9980244
-			//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
-			//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
-			"ll": "37.7749,-122.4194",
-			"query": 'food',
-			// v: "20180323",
-			//"near": "Dallas, Tx"
-	
-  
-			 //   "near": "Dallas, Tx",
-			 // "categoryId": '4d4b7105d754a06374d81259',
-			 // "radius":"250"
-			 //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>})
-		};
-		
-		//axios.get(endPoint + new URLSearchParams(params))
-		//	.then(response => {
-		//		console.log(response);
-		//	})
-		//	.catch(error => {
-		//		console.log("ERROR!! " + error)
-		//	})
-			
-		foursquare.venues.getVenues(params).then((res) => {
-			this.setState({
-				items: res.response.venues
-			});
-			//console.log(this.state);
-		})
-		.catch(error => {
-				console.log("ERROR!! " + error)
-		})
-		//console.log(this.state.items);
 	}
 	
 	updateQuery = (query) =>{
@@ -116,24 +61,9 @@ class App extends Component {
 			previousElement.classList.remove("active");
 		}
 		var params = {
-			// 40.7413549, lng: -73.9980244
-			//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
-			//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
 			"ll": "37.7749,-122.4194",
-			"query": query,
-			// v: "20180323",
-			//"near": "Dallas, Tx"
-	
-  
-		 //   "near": "Dallas, Tx",
-		 // "categoryId": '4d4b7105d754a06374d81259',
-		 // "radius":"250"
-		 //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>}) }
-
+			"query": document.getElementById('search-listings').value,
 		};
-		//this.setState({query: query.trim()})
-		//console.log(params);
-		//Removing the Markers from the map
 		for (var i=0; i<markers.length; i++){
 				markers[i].setMap(null);
 		}
@@ -144,7 +74,6 @@ class App extends Component {
 				items: res.response.venues
 			},this.rendermap()
 			)
-			//console.log(this.state.items);
 		})
 		
 		
@@ -162,13 +91,12 @@ class App extends Component {
 		}
 		
 		markers = [];
-		if(query){
-		const match = new RegExp(escapeRegExp(query), 'i')
+		if(document.getElementById('filter-listings').value){
+		const match = new RegExp(escapeRegExp(document.getElementById('filter-listings').value), 'i')
 			filterMarkers = this.state.items.filter((venue) => match.test(venue.name))
 		}else{
 			filterMarkers = this.state.items;
 		}
-		//showingmarkers = markers.filter((venue) => venue.id === v.id);
 		console.log(filterMarkers);
 		this.rendermap()
 	}
@@ -189,7 +117,7 @@ class App extends Component {
 
 			// Load the Google Maps API
 			const script = document.createElement("script");
-			const API = 'AIzaSyAOoEVFmUBvaqUgrMpSZQEGpgp1x9_y-I4';
+			
 			script.src = `https://maps.googleapis.com/maps/api/js?key=${API}&callback=resolveGoogleMapsPromise`;
 			script.async = true;
 			document.body.appendChild(script);
@@ -211,22 +139,9 @@ class App extends Component {
 		this.handleResize();
 		window.addEventListener('resize', this.handleResize)
 		
-		console.log(this.state.windowWidth);
-
 		var params = {
-			// 40.7413549, lng: -73.9980244
-			//clientID: '00BVPJHFDPUKMUOTIEO4IGK53GUPTYTMVIMUEKHBIIX3DSZO',
-			//clientSecret: 'IKB3WGDPXTVPMEUFWZB1GFHHULK0JE3VTGYG1OYMVRO3LNSK',
 			"ll": "37.7749,-122.4194",
 			"query": '',
-			// v: "20180323",
-			//"near": "Dallas, Tx"
-			
-	  
-			 //   "near": "Dallas, Tx",
-			 // "categoryId": '4d4b7105d754a06374d81259',
-			 // "radius":"250"
-			 //	this.state.items.map(item=> { return<div key={item.id}><p>{item.name}</p></div>}) }
 		};
 		  
 		//Get 4 square Data.
@@ -235,9 +150,7 @@ class App extends Component {
 			this.setState({
 				items: res.response.venues
 			},this.rendermap()
-			);
-			//console.log(this.state.items);
-			
+			);			
 		})
 		.catch(error => {
 				console.log("ERRORQQQQQQQQQQQQQQQQQQQQQQQQ!! " + error)
@@ -292,18 +205,15 @@ class App extends Component {
 				}
 			]
 			center = {lat: 37.7749, lng: -122.4194};
-				//const center = {lat: 40.7413549, lng: -73.9980244};
-				//37.7749,-122.4194"
 			document.getElementById('lat').value = center.lat;
 			document.getElementById('lng').value = center.lng;
 			this.map = new google.maps.Map(document.getElementById('map'), {
 				zoom: 13,
 				center: center,
-				//styles: styles,
+				styles: styles,
 				mapTypeControl: false
 			});
 		})
-		//this.rendermap();
 	}
 	
 	
@@ -311,59 +221,32 @@ class App extends Component {
 		// Once the Google Maps API has finished loading, initialize the map
 		
 		this.getGoogleMaps().then((google) => {
-			//console.log(this.state.items);
-
-			//const center = {lat: 37.7749, lng: -122.4194};
-				//const center = {lat: 40.7413549, lng: -73.9980244};
-				//37.7749,-122.4194"
-			//const map = new google.maps.Map(document.getElementById('map'), {
-			//	zoom: 13,
-			//	center: center,
-				//styles: styles,
-			//	mapTypeControl: false
-			//});
-	  
 			largeInfowindow = new google.maps.InfoWindow();
 		
 			var defaultIcon = makeMarkerIcon('0091ff');
 		
 			var highlightedIcon = makeMarkerIcon('FFFF24');
 		
-			//const marker = new google.maps.Marker({
-			//	position: center,
-			//	map: map
-			//});
-			
-			//var bounds = new google.maps.LatLngBounds();
-			//console.log(this.state.items);
-			//console.log(this.state.items);
-			//console.log(filterMarkers);
 			if(filterMarkers.length === 0){
-				//showingItems=this.state.items;
-				//console.log("state");
-				//this.rendermapOnly();
 				this.setState({
 				showingItems: this.state.items
 			});
 
 			}else{
-				//showingItems = filterMarkers;
-				//console.log("filter");
-				//this.rendermapOnly();
 				this.setState({
 				showingItems: filterMarkers
-			});
-
+				});
 			}
 
-			//console.log(showingItems);
+			if(this.state.showingItems === undefined){
+				this.setState({
+					showingItems: []
+				});
+			}
 			this.state.showingItems.map(venue => {
-			//for (var i=0; i < locations.length; i++) {
-				//location: {lat: 40.7713024, lng: -73.9632393}
-				//var position = locations[i].location;
 				var position = {lat: venue.location.lat,lng:venue.location.lng};
 				
-				var title = venue.name; //locations[i].title;
+				var title = venue.name;
 				
 				marker = new google.maps.Marker({
 					position: position,
@@ -375,10 +258,7 @@ class App extends Component {
 					map:this.map
 				});
 			
-				markers.push(marker);
-				
-				//bounds.extend(marker.position);
-				
+				markers.push(marker);				
 				
 				marker.addListener('click', function() {
 					populateInfoWindow(this, largeInfowindow);
@@ -394,14 +274,10 @@ class App extends Component {
 			
 			
 			function populateInfoWindow(marker, infowindow) {
-				//console.log(marker);
-				//console.log(infowindow);
-				//if (infowindow.marker !== marker) {
 					infowindow.setContent('');
 					infowindow.marker = marker;
 					
 					infowindow.addListener('closeclick', function(){
-						//infowindow.setMarker(null);
 					});
 					var streetViewService = new google.maps.StreetViewService();
 					var radius = 50;
@@ -409,16 +285,12 @@ class App extends Component {
 					
 					
 					function getStreetView(data, status){
-						//console.log();
 						if (status === google.maps.StreetViewStatus.OK){
 							var nearStreetViewLocation = data.location.latLng;
 							var heading = google.maps.geometry.spherical.computeHeading(
 								nearStreetViewLocation, marker.position);
-							//console.log(markers);
-							//var displayvenue = markers.filter((venue) => venue.id === marker.id);
-							//console.log(displayvenue);
+
 							infowindow.setContent('<div>' + marker.title +'</div>'+ marker.address+'<div id="pano"></div>');
-							//console.log(infowindow);
 							var panoramaOptions = {
 								position: nearStreetViewLocation,
 								pov: {
@@ -428,9 +300,7 @@ class App extends Component {
 							};
 							var panorama = new google.maps.StreetViewPanorama(
 								document.getElementById('pano'), panoramaOptions);
-								//console.log(panorama);
 							}else {
-								//console.log(marker);
 								infowindow.setContent('<div>' + marker.title + '</div>' +
 								'<div>No Street View Found</div>');
 							}
@@ -439,7 +309,6 @@ class App extends Component {
 						streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
 						
 						infowindow.open(map, marker);
-				//}
 			}
 		
 			function makeMarkerIcon(markerColor){
@@ -449,7 +318,6 @@ class App extends Component {
 					new google.maps.Point(0,0),
 					new google.maps.Point(10, 34),
 					new google.maps.Size(21, 34));
-					//console.log(markerImage);
 					return markerImage;
 			}
 		});
@@ -457,29 +325,21 @@ class App extends Component {
 	}
 	
 	onPress(v,i){
-		console.log(i.currentTarget);
 		if(previousElement !== "" && previousElement !== undefined){
 			previousElement.classList.remove("active");
 		}
-		//console.log(e);
-		//console.log(this.populateInfoWindow);
 		var displaymarker = markers.filter((venue) => venue.id === v.id);
-		//this.state.items.map((venue) =>(
-		//console.log(displaymarker);
 		this.populateInfoWindow(displaymarker[0],largeInfowindow);
-		//e.target.element.class="newGreenColor";
 		i.currentTarget.classList.add('active');
 		previousElement = i.currentTarget;
 	
 	}
 	
 	populateInfoWindow(marker, infowindow) {
-				//if (infowindow.marker !== marker) {
 					infowindow.setContent('');
 					infowindow.marker = marker;
 					
 					infowindow.addListener('closeclick', function(){
-						//infowindow.setMarker(null);
 					});
 					var streetViewService = new google.maps.StreetViewService();
 					var radius = 50;
@@ -487,14 +347,11 @@ class App extends Component {
 					
 					
 					function getStreetView(data, status){
-						//console.log();
 						if (status === google.maps.StreetViewStatus.OK){
 							var nearStreetViewLocation = data.location.latLng;
 							var heading = google.maps.geometry.spherical.computeHeading(
 								nearStreetViewLocation, marker.position);
-							//console.log(heading);
 							infowindow.setContent('<div>' + marker.title + '</div>'+ marker.address+'<div id="pano"></div>');
-							//console.log(infowindow);
 							var panoramaOptions = {
 								position: nearStreetViewLocation,
 								pov: {
@@ -504,18 +361,14 @@ class App extends Component {
 							};
 							var panorama = new google.maps.StreetViewPanorama(
 								document.getElementById('pano'), panoramaOptions);
-								//console.log(panorama);
 							}else {
-								//console.log(marker);
 								infowindow.setContent('<div>' + marker.title + '</div>' +
 								'<div>No Street View Found</div>');
 							}
 						}
 						
 						streetViewService.getPanoramaByLocation(marker.position, radius, getStreetView);
-						
 						infowindow.open(map, marker);
-				//}
 	}
 
 	render() {
@@ -527,7 +380,6 @@ class App extends Component {
 				<div className="App">
 					<div id='map'></div>
 					<div className='options-box'>
-					{this.state.windowWidth} x {this.state.windowHeight}
 						<h1>Options Menu</h1>
 						<div>
 							<div>
